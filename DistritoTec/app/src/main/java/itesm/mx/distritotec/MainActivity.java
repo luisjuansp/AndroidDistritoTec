5b5389b187169c42ceb3826153bdf0f859c4fab5
luisjuansp <luisjuansp@gmail.com>
2015-03-27 01:25:56 -0600

    Google Maps Activity

--- DistritoTec/app/src/main/java/itesm/mx/distritotec/MainActivity.java
+++ DistritoTec/app/src/main/java/itesm/mx/distritotec/MainActivity.java
@@ -1,17 +1,30 @@
 package itesm.mx.distritotec;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    Button map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        map = (Button) findViewById(R.id.locationMainBtn);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
