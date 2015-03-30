05ce58274c084cfeecf38e8613b1a2017cd2ac48
Juan <juanm.flores127@gmail.com>
2015-03-30 17:04:26 -0600

    gpl license

--- DistritoTec/app/src/main/java/itesm/mx/distritotec/MapsActivity.java
+++ DistritoTec/app/src/main/java/itesm/mx/distritotec/MapsActivity.java
@@ -27,7 +27,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 
+/*This file is part of DistritoTec.
 
+        DistritoTec is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        DistritoTec is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with DistritoTec.  If not, see <http://www.gnu.org/licenses/>.*/
 public class MapsActivity extends FragmentActivity implements ConnectionCallbacks, OnConnectionFailedListener {
 
     protected static final String TAG = "basic-location-sample";
